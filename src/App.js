69ca65bbec9c71356827197e9a39b46744921066
OmerAbdulSamad110 <41125159+OmerAbdulSamad110@users.noreply.github.com>
2020-05-16 08:54:58 +0500

    setup main routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
+import { Route, Switch } from 'react-router';
+import Orders from './containers/Orders/Orders';
 
 function App() {
   return (
     <div>
       <Layout>
-        <BurgerBuilder />
+        <Switch>
+          {/* if extended path for new component like /checkout/delivery
+          do not use exact in rout then */}
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
+          <Route path="/" exact component={BurgerBuilder} />
+        </Switch>
       </Layout>
     </div>
   );
